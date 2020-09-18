9913082d816a9b9f586dfabc9c9007ccf1bd6855
Victor Campos <campos.viictor@gmail.com>
2020-09-18 12:30:54 -0300

    criando seeds - npm faker

---
+++ seeds.js
@@ -0,0 +1,66 @@
+const faker = require('faker')
+const { hash } = require('bcryptjs')
+
+const User = require('./src/app/models/User')
+const File = require('./src/app/models/File')
+const Product = require('./src/app/models/Product')
+
+let usersId = []
+let totalProducts = 10
+let totalUsers = 3
+
+async function createUsers() {
+    const users = []
+    const password = await hash('12345', 8)
+    
+    while(users.length < totalUsers) {
+        users.push({
+            name: faker.name.firstName(),
+            email: faker.internet.email(),
+            password,
+            cpf_cnpj: faker.random.number(99999999999),
+            cep: faker.random.number(99999999),
+            address: faker.address.streetName()
+        })
+    }
+    const usersPromise = users.map(user => User.create(user))
+
+    usersId = await Promise.all(usersPromise)
+}
+
+async function createProducts() {
+    let products = []
+
+    while(products.length < totalProducts) {
+        products.push({
+            category_id: Math.ceil(Math.random() * 5),
+            user_id: usersId[Math.floor(Math.random() * totalUsers)],
+            name: faker.name.title(),
+            description: faker.lorem.paragraph(Math.ceil(Math.random * 10)),
+            old_price: faker.random.number(999),
+            price: faker.random.number(9999),
+            quantity: faker.random.number(99),
+            status: Math.round(Math.random())
+        })
+    }
+    const productsPromise = products.map(product => Product.create(product))
+    productsId = await Promise.all(productsPromise)
+
+    let files = []
+    while(files.length < 50) {
+        files.push({
+            name: faker.image.image(),
+            path: `public/images/placeholder.png`,
+            product_id: productsId[Math.floor(Math.random() * totalProducts)]
+        })
+    }
+    const filesPromise = files.map(file => File.create(file))
+    await Promise.all(filesPromise)
+}
+
+async function init() {
+    await createUsers()
+    await createProducts()
+}
+
+init()
