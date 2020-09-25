5d583a0d8f35f452de782fe88df12bb289619eae
Victor Campos <campos.viictor@gmail.com>
2020-09-25 09:19:41 -0300

    criando estrutura back-end das ordens de pedidos

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,7 +1,10 @@
 const LoadService = require('../services/LoadProductService')
 const User = require('../models/User')
+const Order = require('../models/Order')
 
 const mailer = require('../../lib/mailer')
+const { formatPrice, date } = require('../../lib/utils')
+const Cart = require('../../lib/cart')
 
 const email = (seller, product, buyer) => `
 <h2>Olá, ${seller.name}</h2>
@@ -23,23 +26,107 @@ const email = (seller, product, buyer) => `
 `
 
 module.exports = {
+    async index(req, res) {
+        // PEGAR OS PEDIDOS DO USUÁRIO
+        let orders = await Order.findAll({ where: { buyer_id: req.session.userId}})
+
+        const getOrdersPromise = orders.map(async order => {
+            // DETALHES DO PRODUTO
+            order.product = await LoadService.load('products', {
+                where: { id: order.product_id}
+            })
+
+            // DETALHES DO COMPRADOR 
+            order.buyer = await User.findOne({
+                where: { id: order.buyer_id }
+            })
+
+            // DETALHES DO VENDEDOR
+            order.seller = await User.findOne({
+                where: { id: order.seller_id }
+            }) 
+
+            // FORMATAÇÃO DE PREÇO
+            order.formatedPrice = formatPrice(order.price) 
+            order.formatedTotal = formatPrice(order.total) 
+
+            // FORMATAÇÃO DO STATUS 
+            const orderStatus = {
+                open: 'Aberto',
+                sold: 'Vendido',
+                canceled: 'Cancelado'
+            }
+
+            order.formatedStatus = orderStatus[order.status]
+
+            // FORMATAÇÃO DE ATUALIZAÇÃO
+            const updatedAt = date(order.updated_at)
+            const updatedDate = `${updatedAt.day}/${updatedAt.month}/${updatedAt.year}`
+            order.formatedUpdatedAt = `${order.formatedStatus} em ${updatedDate} às ${updatedAt.hour}h${updatedAt.minutes}`
+
+            return order
+        })
+
+        orders = await Promise.all(getOrdersPromise)
+
+        return res.render('order/index', { orders })
+    },
     async post(req, res) {
         try {
-            const product = await LoadService.load('product', {
-                where: {id: req.body.id}
+            // PEGAR OS PRODUTOS DO CARRINHO
+            const cart = Cart.init(req.session.cart)
+
+            // VERIFICAR SE TODOS OS PEDIDOS SÃO VÁLIDOS
+            const buyer_id = req.session.userId
+            const createOrdersPromise = cart.items
+            .filter(item => item.product.user_id != buyer_id)
+            .map(async item => {
+                let { product, price: total, quantity } = item
+                const { price, id: product_id, user_id: seller_id } = product
+                const status = 'open'
+
+                // CRIAR ORDEM DO PEDIDO
+                const order = await Order.create({
+                    seller_id,
+                    buyer_id,
+                    product_id,
+                    seller_id,
+                    price,
+                    total,
+                    quantity, 
+                    status
+                })
+
+                // PEGAR DADOS DO PRODUTO
+                product = await LoadService.load('product', {
+                    where: {id: product_id}
+                })
+
+                // PEGAR DADOS DO VENDEDOR
+                const seller = await User.findOne({where: {id: seller_id}})
+
+                // PEGAR DADOS DO COMPRADOR
+                const buyer = await User.findOne({where: {id: buyer_id}})
+
+                // ENVIAR EMAIL COM DADOS DA COMPRA PARA O VENDEDOR
+                await mailer.sendMail({
+                    to: seller.email,
+                    from: 'no-reply@launchstore.com.br',
+                    subject: 'Novo pedido de compra',
+                    html: email(seller, product, buyer)
+                })
+
+                return order
+
             })
 
-            const seller = await User.findOne({where: {id: product.user_id}})
+            await Promise.all(createOrdersPromise)
 
-            const buyer = await User.findOne({where: {id: req.session.userId}})
+            // LIMPAR CARRINHO
+            delete req.session.cart
+            Cart.init()
 
-            await mailer.sendMail({
-                to: seller.email,
-                from: 'no-reply@launchstore.com.br',
-                subject: 'Novo pedido de compra',
-                html: email(seller, product, buyer)
-            })
-
+            // NOTIFICAR O USUÁRIO COM UMA MENSAGEM DE SUCESSO
             return res.render('order/success')
         } catch (error) {
             console.error(error)
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,16 @@ const HomeController = require ('../app/controllers/HomeController')
 
 const users = require('./users')
 const products = require('./products')
+const cart = require('./cart')
+const orders = require('./orders')
 
 routes  
 .get ('/', HomeController.index)
 
 .use('/users', users)
 .use('/products', products)
+.use('/cart', cart)
+.use('/orders', orders)
 
 
 
